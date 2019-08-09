5f7ecf4c94d252670de6ddff53da447c5d365436
carloscn90 <carloscastanedan@gmail.com>
2019-08-09 17:34:32 -0500

    updated: animations added

--- src/components/ProductDetail/index.js
+++ src/components/ProductDetail/index.js
@@ -2,21 +2,21 @@ import React from 'react'
 import './styles.css'
 
 const ProductDetail = ({ product }) => (
-  <article className="product-wrapper">
-    <div className="image-container">
-      <img
-        src={require(`../../assets/images/${product.image}`)}
-        alt={product.name}
-      />
-    </div>
-    <div className="product-description">
-      <h2>{product.name}</h2>
-      <p>{product.summary}</p>
-    </div>
-    <div className="product-summary">
-      <div>look</div>
-      <div>{product._id}</div>
-    </div>
+  <article className="product-wrapper" key={product._id}>
+      <div className="image-container">
+        <img
+          src={require(`../../assets/images/${product.image}`)}
+          alt={product.name}
+        />
+      </div>
+      <div className="product-description">
+        <h2>{product.name}</h2>
+        <p>{product.summary}</p>
+      </div>
+      <div className="product-summary">
+        <div>look</div>
+        <div>{product._id}</div>
+      </div>
   </article>
 )
 
